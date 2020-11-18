a6877dc9a460b78c912515575a0f05542d45b086
matin <behz0003@algonquinlive.com>
2020-11-18 00:21:50 -0500

    modification

--- app/src/main/java/com/example/androidlabs/DetailsFragment.java
+++ app/src/main/java/com/example/androidlabs/DetailsFragment.java
@@ -1,34 +1,39 @@
 package com.example.androidlabs;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link DetailsFragment#newInstance} factory method to
+ * Use the  factory method to
  * create an instance of this fragment.
  */
 public class DetailsFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    /** private static final String ARG_PARAM1 = "param1";
+     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+     // TODO: Rename and change types of parameters
+     private String mParam1;
+     private String mParam2;
 
-    public DetailsFragment() {
-        // Required empty public constructor
-    }
+     public DetailsFragment() {
+     // Required empty public constructor
+     }
 
-    /**
+     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
@@ -37,28 +42,55 @@ public class DetailsFragment extends Fragment {
      * @return A new instance of fragment DetailsFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static DetailsFragment newInstance(String param1, String param2) {
-        DetailsFragment fragment = new DetailsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    /**  public static DetailsFragment newInstance(String param1, String param2) {
+     DetailsFragment fragment = new DetailsFragment();
+     Bundle args = new Bundle();
+     args.putString(ARG_PARAM1, param1);
+     args.putString(ARG_PARAM2, param2);
+     fragment.setArguments(args);
+     return fragment;
+     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+     @Override
+     public void onCreate(Bundle savedInstanceState) {
+     super.onCreate(savedInstanceState);
+     if (getArguments() != null) {
+     mParam1 = getArguments().getString(ARG_PARAM1);
+     mParam2 = getArguments().getString(ARG_PARAM2);
+     }
+     }
+
+     @Override
+     public View onCreateView(LayoutInflater inflater, ViewGroup container,
+     Bundle savedInstanceState) {
+     // Inflate the layout for this fragment
+     return inflater.inflate(R.layout.fragment_details, container, false);
+     }
+     }*/
+
+    private AppCompatActivity parentActivity;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_details, container, false);
+        Bundle data = getArguments();
+        long id = data.getLong("ID");
+        String message = data.getString("MESSAGE");
+        boolean isSend = data.getBoolean("IS_SEND");
+
+        View result = inflater.inflate(R.layout.fragment_details,container,false);
+        TextView messageBox = result.findViewById(R.id.message);
+        messageBox.setText(message);
+        TextView idBox = result.findViewById(R.id.idText);
+        idBox.setText("ID = " + id);
+        CheckBox box = result.findViewById(R.id.checkBox);
+        box.setChecked(isSend);
+
+        Button hide = result.findViewById(R.id.HideButton);
+        hide.setOnClickListener( e -> {
+            parentActivity.getSupportFragmentManager().beginTransaction().remove(this).commit();
+        });
+
+        return result;
     }
 }
